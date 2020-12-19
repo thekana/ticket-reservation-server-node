7c545dfd8f9b14919e467b3c805476caeab2727c
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2020-12-19 15:47:17 +0700

    updated docker scripts

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec -i test_ts_postgres psql -U postgres -c "drop database if exists test" && \
-docker exec -i test_ts_postgres psql -U postgres -c "create database test"
+docker exec -i ticket_postgres psql -U postgres -c "drop database if exists ticket" && \
+docker exec -i ticket_postgres psql -U postgres -c "create database ticket"
--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name test_ts_postgres postgres:12-alpine
+docker run -d --rm -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name ticket_postgres postgres:12-alpine
